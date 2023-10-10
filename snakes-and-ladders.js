440c12e37607616e6c0bad1c21460327789db3a0
Adeesh Garg <adeesh.g47@gmail.com>
2023-10-10 23:38:12 +0530

    Adding some functions

--- snakes-and-ladders.js
+++ snakes-and-ladders.js
@@ -1,4 +1,10 @@
-const Sanke_And_Ladders = (players) => {
+const Sanke_And_Ladders = (playersName) => {
+
+    let currentPlayer = 0
+    let rank = []
+
+    rank.push("Ranking List")
+
 
     const board = []
     for(let i = 1; i <= 100; i++) {
@@ -33,15 +39,61 @@ const Sanke_And_Ladders = (players) => {
     ladders.push(ladder(41, 79))
     ladders.push(ladder(54, 88))
 
-    let player = (name) => {
+    const player = (name) => {
         name = name,
         position = 1
     }
 
     const players = []
 
-    players.forEach(player => {
+    playersName.forEach(player => {
         players.push(player(player))
     });
 
+    function diceRoll(){
+        return Math.floor(Math.random() * 6) + 1
+    }
+
+    function isGameOver(){
+        let count = 0
+        players.forEach(player => {
+            if(player.position === 100){
+                count++
+            }
+        })
+        return count === players.length - 1
+    }
+
+    function updatePosition(playerPosition, number){
+        players[playerPosition].position += number
+        if(players[playerPosition].position > 100){
+            players[playerPosition].position -= number
+            return
+        }
+        if(players[playerPosition].position === 100){
+            rank.push(players[playerPosition].name)
+            players.splice(playerPosition, 1)
+            return
+        }
+        snakes.forEach(snake => {
+            if(snake.head === players[playerPosition].position){
+                players[playerPosition].position = snake.tail
+            }
+        })
+        ladders.forEach(ladder => {
+            if(ladder.start === players[playerPosition].position){
+                players[playerPosition].position = ladder.end
+            }
+        })
+    }
+
+    function play(){
+        if (isGameOver()) {
+            return rank
+        }
+        let randomNumber = diceRoll()
+        updatePosition(currentPlayer, randomNumber)
+        currentPlayer = currentPlayer === players.length - 1 ? 0 : currentPlayer + 1
+        return rank, players
+    }
 }
